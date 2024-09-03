6b3d96e50d8472fcf7f6249917e081276bfd5c6d
Seohong Park <artberryx@gmail.com>
2024-09-03 10:13:14 -0700

    Change env names

--- envs/powderworld/__init__.py
+++ envs/powderworld/__init__.py
@@ -1,21 +1,21 @@
 from gymnasium.envs.registration import register
 
 register(
-    id='powderworld-2-v0',
+    id='powderworld-easy-v0',
     entry_point='envs.powderworld.powderworld_env:PowderworldEnv',
     max_episode_steps=500,
     kwargs=dict(num_elems=2),
 )
 
 register(
-    id='powderworld-5-v0',
+    id='powderworld-medium-v0',
     entry_point='envs.powderworld.powderworld_env:PowderworldEnv',
     max_episode_steps=500,
     kwargs=dict(num_elems=5),
 )
 
 register(
-    id='powderworld-8-v0',
+    id='powderworld-hard-v0',
     entry_point='envs.powderworld.powderworld_env:PowderworldEnv',
     max_episode_steps=500,
     kwargs=dict(num_elems=8),
